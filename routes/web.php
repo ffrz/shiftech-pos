0a1655a2fde052648f42626ae96248151e9d4459
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-08-11 07:33:55 +0700

    redirect / to /admin

--- routes/web.php
+++ routes/web.php
@@ -41,10 +41,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::redirect('/', '/admin');
 Route::redirect('/admin', '/admin/dashboard');
 
 Route::middleware([OnlyGuest::class])->group(function () {
